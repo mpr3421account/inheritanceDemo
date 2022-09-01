3fdfea9dc459f0032f66b30312a4595eba70b853
Marcos Paulo <itsolutions031250@gmail.com>
2022-09-01 17:49:12 -0300

    Polymorphism

--- src/application/inheritanceDemo.java
+++ src/application/inheritanceDemo.java
@@ -2,7 +2,7 @@ package application;
 
 public class inheritanceDemo {
     public static void main(String[] args) {
-        NormalMember mem1 = new NormalMember("James", 1, 2010);
+        /*NormalMember mem1 = new NormalMember("James", 1, 2010);
         VIPMember mem2 = new VIPMember("Andy", 2, 2011);
 
         mem1.calculateAnnualFee();
@@ -13,8 +13,23 @@ public class inheritanceDemo {
 
         mem1.setDiscount();
         mem1.calculateAnnualFee();
-        mem1.displayMemInfo();
+        mem1.displayMemInfo();*/
 
+
+        //Polymorphism
+
+        Member[] clubMember = new Member[6];
+        clubMember[0] = new NormalMember("James", 1, 2010);
+        clubMember[1] = new NormalMember("Andy", 2, 2011);
+        clubMember[2] = new NormalMember("Bill", 3, 2011);
+        clubMember[3] = new VIPMember("Carol", 4, 2012);
+        clubMember[4] = new VIPMember("Evelyn", 5, 2012);
+        clubMember[5] = new Member("Yvone", 6, 2013);
+
+        for(Member m:clubMember){
+            m.calculateAnnualFee();
+            m.displayMemInfo();
+        }
 
 
 
